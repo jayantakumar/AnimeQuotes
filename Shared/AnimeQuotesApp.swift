f31ef1f4b2993e00e09638a8ffedfdf39f879d9c
Jayantakumar M <benten867@gmail.com>
2022-02-11 22:08:14 +0530

    Initial Commit

---
+++ Shared/AnimeQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeQuotesApp.swift
+//  Shared
+//
+//  Created by Jayanta kumar on 11/02/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimeQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
